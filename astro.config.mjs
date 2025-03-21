f64f3429fb3fabd2415e5544e505c3e197641995
htmlh4cker <114673180+htmlh4cker@users.noreply.github.com>
2025-03-21 13:06:53 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://htmlh4cker.github.io',
-  	base: '/htmlh4ckerWebsite',
-	vite: {
-		plugins: [tailwindcss()]
-	}
+  site: 'https://htmlh4cker.github.io',
+  base: '/htmlh4ckerWebsite',
+  vite: {
+    plugins: [
+      tailwindcss({
+        plugins: [
+          require('tailwindcss-animated')
+        ]
+      })
+    ]
+  }
 });
